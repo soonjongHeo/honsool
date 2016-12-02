238c8d5f3435a660438d9ea586a47ceb47c9d48e
soonjongHeo <soon9086@gmail.com>
2016-12-02 18:33:38 +0900

    project

---
+++ Cock_Tail/Assets/1.Scripts/SelfRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotation : MonoBehaviour {
+
+	public int selfyRotateSpeed;
+	
+	void Update () {
+		gameObject.transform.Rotate (0, selfyRotateSpeed * Time.deltaTime, 0);
+	}
+}
